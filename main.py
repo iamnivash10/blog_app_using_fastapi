da29e367e9a15890c54b652c28f60b46132d4305
iamnivash10 <iamnivash.b@gmail.com>
2024-10-16 14:12:24 +0530

    learn_fastapi

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"data": {"message": "Hello World"}}
+
+@app.get("/about")
+async def about():
+    return {"data": "About Page"}
+
+@app.get("/about/{id}")
+async def ab(id:int):
+    return {"data": id}
